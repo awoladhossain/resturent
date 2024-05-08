fc0184388f2aa0cab44f6e97e6d35600594017ff
unknown <awoladh04@gmail.com>
2024-05-08 11:04:25 +0600

    admin working

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://****:****@cluster0.secxzoo.mongodb.net/food-del"
+      "mongodb+srv://fooddb:Df6wK77elbbkzKIb@cluster0.secxzoo.mongodb.net/food-del"
     )
     .then(() => {
       console.log("DB is Connected");
     });
 };
 
+//   ***
