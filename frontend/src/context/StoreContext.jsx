8e00e515d8267a7bd5e6b11aa1ec336121949f56
unknown <awoladh04@gmail.com>
2024-05-13 23:19:48 +0600

    cart data loading

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/food del assets/frontend_assets/assets";
+// import { food_list } from "../assets/food del assets/frontend_assets/assets";
+import axios from "axios";
 
 export const StoreContext = createContext(null);
 
@@ -8,6 +9,7 @@ const StoreContextProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState({});
   const url = "http://localhost:4000";
   const [storeToken, setStoreToke] = useState("");
+  const [food_list, setFoodList] = useState([]);
 
   // Load cart items from local storage
   // const loadCartItems = () => {
@@ -51,12 +53,20 @@ const StoreContextProvider = ({ children }) => {
     return totalAmount;
   };
 
+  const fetchFoodList = async () => {
+    const response = await axios.get(url + "/api/food/list");
+    setFoodList(response.data.data);
+  };
+
   useEffect(() => {
-    if (localStorage.getItem("token")) {
-      setStoreToke(localStorage.getItem("token"));
+    async function loadData() {
+      await fetchFoodList();
+      if (localStorage.getItem("token")) {
+        setStoreToke(localStorage.getItem("token"));
+      }
     }
+    loadData();
   }, []);
-  
 
   const contextValue = {
     food_list,
